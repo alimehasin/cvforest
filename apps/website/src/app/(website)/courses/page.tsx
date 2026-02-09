5567f7dc25d51b5ae3ab09ee58aec47f1ce7e1e3
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2026-02-10 00:54:00 +0300

    refactor: update translations for 'coming soon' in courses page and restructure i18n files for consistency

--- apps/website/src/app/(website)/courses/page.tsx
+++ apps/website/src/app/(website)/courses/page.tsx
@@ -2,11 +2,11 @@ import { Center, Title } from '@mantine/core';
 import { getTranslations } from 'next-intl/server';
 
 export default async function CoursesPage() {
-  const t = await getTranslations('courses');
+  const t = await getTranslations();
 
   return (
     <Center py={164}>
-      <Title fz={96}>{t('comingSoon')}</Title>
+      <Title fz={96}>{t('courses.comingSoon')}</Title>
     </Center>
   );
 }
