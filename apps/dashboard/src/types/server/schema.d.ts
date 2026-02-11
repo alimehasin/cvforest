0e305c2176159e917349eaea78370a5795bfa7cd
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2026-02-12 00:15:15 +0300

    refactor: remove unused API paths from server schema to streamline type definitions

--- apps/dashboard/src/types/server/schema.d.ts
+++ apps/dashboard/src/types/server/schema.d.ts
@@ -1417,6 +1417,19 @@ export interface components {
       }[];
     };
     UserCvsCreateBody: {
+      profile?: {
+        name?: string;
+        /** Format: email */
+        email?: string;
+        /** @description يجب ادخال رقم هاتف عراقي صحيح مثال +9647701234567 */
+        phoneNumber?: string;
+        /** @enum {string} */
+        gender?: 'Male' | 'Female';
+        birthDate?: Record<string, never> | string | number;
+        /** Format: uuid */
+        avatarId?: string;
+        governorateId?: (string | null) | null;
+      };
       jobTitle: string;
       experienceInYears: number;
       expectedSalaryMin?: number;
@@ -1436,19 +1449,6 @@ export interface components {
       portfolioUrl?: string;
       availableForHire: boolean;
       skillIds: string[];
-      profile?: {
-        name?: string;
-        /** Format: email */
-        email?: string;
-        /** @description يجب ادخال رقم هاتف عراقي صحيح مثال +9647701234567 */
-        phoneNumber?: string;
-        /** @enum {string} */
-        gender?: 'Male' | 'Female';
-        birthDate?: Record<string, never> | string | number;
-        /** Format: uuid */
-        avatarId?: string;
-        governorateId?: (string | null) | null;
-      };
     };
     UserCvsCreateResponse: {
       id: string;
